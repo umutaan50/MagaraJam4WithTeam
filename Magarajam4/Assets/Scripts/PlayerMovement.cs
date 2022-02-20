77b8284f78415a7b62d485c5fcfeb1ddb00ada4e
thelastnobody7372 <thelastnobody00@gmail.com>
2022-02-20 17:15:13 +0300

    Update PlayerMovement.cs

--- Magarajam4/Assets/Scripts/PlayerMovement.cs
+++ Magarajam4/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,6 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 8;
     public float horizontalInput;
     public float jumpForce = 10;
-    // public bool isOnGround = true;
     private BoxCollider2D boxCollider2d;
 
     private Rigidbody2D playerRigidbody;
@@ -22,36 +21,18 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody = transform.GetComponent<Rigidbody2D>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        // Horizontal player movement.
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
-
-        // Player jump.
+        Debug.Log(IsGrounded());
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            // Debug.Log("Should've worked.");
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    //private void OnCollisionEnter2D(Collision collision)
-    //{
 
-    //    Debug.Log("Collision worked without ground");
-    //    if (collision.gameObject.CompareTag("Ground"))
-    //    {
-    //        Debug.Log("Collision worked with ground");
-    //        isOnGround = true;
-    //    }
-    //}
 
     private bool IsGrounded()
     {
