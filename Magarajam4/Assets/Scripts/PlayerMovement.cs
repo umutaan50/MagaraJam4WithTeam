cf951d270c7a1bf7fa63178d2f07a4f077644034
Umut Kaan <74989036+umutaan50@users.noreply.github.com>
2022-02-20 17:08:17 +0300

    Solved issue with jumping
    Took help from CodeMonkey's Simple Jump in Unity 2D tutorial.

--- Magarajam4/Assets/Scripts/PlayerMovement.cs
+++ Magarajam4/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformsLayerMask;
     public float speed = 8;
     public float horizontalInput;
     public float jumpForce = 10;
-    public bool isOnGround = true;
+    // public bool isOnGround = true;
+    private BoxCollider2D boxCollider2d;
 
     private Rigidbody2D playerRigidbody;
 
 
+    private void Awake()
+    {
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        playerRigidbody = transform.GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -28,21 +35,28 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
 
         // Player jump.
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            isOnGround = false;
+            // Debug.Log("Should've worked.");
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    private void OnCollisionEnter2D(Collision collision)
-    {
+    //private void OnCollisionEnter2D(Collision collision)
+    //{
 
-        Debug.Log("Collision worked without ground");
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            Debug.Log("Collision worked with ground");
-            isOnGround = true;
-        }
+    //    Debug.Log("Collision worked without ground");
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        Debug.Log("Collision worked with ground");
+    //        isOnGround = true;
+    //    }
+    //}
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
     }
 }
